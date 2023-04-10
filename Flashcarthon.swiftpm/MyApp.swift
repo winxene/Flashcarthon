b205e00765083ae1409dc817224d9bcfac15b61a
winxene <winxeneric@gmail.com>
2023-04-11 00:39:06 +0700

    feat: added FlashCardViewto show a page specifically for flashcards

--- Flashcarthon.swiftpm/MyApp.swift
+++ Flashcarthon.swiftpm/MyApp.swift
@@ -5,9 +5,12 @@ struct MyApp: App {
 
     var body: some Scene {
         WindowGroup {
-            let manager = ProvinceCityManager()
-            let randomProvince =  manager.getRandomProvince()
-            FlashCard(question: "What is the capital city of \(randomProvince.province)", answer: randomProvince.capital)
+//            let manager = ProvinceCityManager()
+//            let randomProvince =  manager.getRandomProvince()
+//            FlashCard(question: "What is the capital city of \(randomProvince.province)", answer: randomProvince.capital)
+//            let randomProvince2 = manager.getRandomProvince()
+//            FlashCard(question: "What is the capital city of \(randomProvince2.province)", answer: randomProvince2.capital)
+            FlashCardView()
         }
     }
 }
