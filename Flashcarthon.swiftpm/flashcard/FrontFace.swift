c4d891026cf3376c439c8359f2466956705101f1
winxene <winxeneric@gmail.com>
2023-04-10 23:26:49 +0700

    fix: added warp text so that the answer text doesn't go out of the boarder

--- Flashcarthon.swiftpm/flashcard/FrontFace.swift
+++ Flashcarthon.swiftpm/flashcard/FrontFace.swift
@@ -13,8 +13,10 @@ struct FrontFace: View {
                 .frame(width: width, height: height)
                 .shadow(color: .gray, radius: 2, x: 0, y: 0)
             VStack {
-                Text("Answer:\n").font(.headline).padding(.bottom, 8)
+                Text("Answer:").font(.headline).padding(.bottom, 8)
                 Text(answer)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 200)
             }
         }.rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
     }
