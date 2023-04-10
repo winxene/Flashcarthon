ccf8fbf456566fd522667d56ec459aec13d448b5
winxene <winxeneric@gmail.com>
2023-04-11 00:07:19 +0700

    feat: added Models and implementing it

---
+++ Flashcarthon.swiftpm/Models/ProvinceCityManager.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+class ProvinceCityManager {
+    let provincesAndCapitals = [
+        "Aceh": "Banda Aceh",
+        "Bali": "Denpasar",
+        "Bangka Belitung": "Pangkal Pinang",
+        "Banten": "Serang",
+        "Bengkulu": "Bengkulu",
+        "Gorontalo": "Gorontalo",
+        "Jakarta": "Jakarta",
+        "Jambi": "Jambi",
+        "Jawa Barat": "Bandung",
+        "Jawa Tengah": "Semarang",
+        "Jawa Timur": "Surabaya",
+        "Kalimantan Barat": "Pontianak",
+        "Kalimantan Selatan": "Banjarmasin",
+        "Kalimantan Tengah": "Palangkaraya",
+        "Kalimantan Timur": "Samarinda",
+        "Kalimantan Utara": "Tanjung Selor",
+        "Kepulauan Riau": "Tanjung Pinang",
+        "Lampung": "Bandar Lampung",
+        "Maluku": "Ambon",
+        "Maluku Utara": "Sofifi",
+        "Nusa Tenggara Barat": "Mataram",
+        "Nusa Tenggara Timur": "Kupang",
+        "Papua": "Jayapura",
+        "Papua Barat": "Manokwari",
+        "Riau": "Pekanbaru",
+        "Sulawesi Barat": "Mamuju",
+        "Sulawesi Selatan": "Makassar",
+        "Sulawesi Tengah": "Palu",
+        "Sulawesi Tenggara": "Kendari",
+        "Sulawesi Utara": "Manado",
+        "Sumatera Barat": "Padang",
+        "Sumatera Selatan": "Palembang",
+        "Sumatera Utara": "Medan",
+    ]
+    
+    func getRandomProvince() -> (province: String, capital: String) {
+        let index = Int.random(in: 0..<provincesAndCapitals.count)
+        let province = Array(provincesAndCapitals.keys)[index]
+        let capital = Array(provincesAndCapitals.values)[index]
+        return (province, capital)
+    }
+}
--- Flashcarthon.swiftpm/MyApp.swift
+++ Flashcarthon.swiftpm/MyApp.swift
@@ -2,16 +2,12 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+
     var body: some Scene {
         WindowGroup {
-            FlashCard(question: "What is the capital city of France", answer: "Paris")
-            FlashCard(question: "What is the capital city of Italy", answer: "Rome")
+            let manager = ProvinceCityManager()
+            let randomProvince =  manager.getRandomProvince()
+            FlashCard(question: "What is the capital city of \(randomProvince.province)", answer: randomProvince.capital)
         }
-    }
-}
-
-struct Previews_MyApp_Previews: PreviewProvider {
-    static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
