000fe7b9483db30538e337be7c9b4b03927f6c39
winxene <winxeneric@gmail.com>
2023-04-16 23:58:27 +0700

    feat: added home screen

--- Flashcarthon.swiftpm/home/HomeView.swift
+++ Flashcarthon.swiftpm/home/HomeView.swift
@@ -1,3 +1,30 @@
-//import SwiftUI
+import SwiftUI
 
-
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("Flashcarthon")
+                    .font(.system(size: 48, weight: .bold)) 
+                NavigationLink(destination: FlashCardView()) {
+                    Text("Flashcards")
+                        .font(.title)
+                        .padding()
+                        .background(Color.brown)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                NavigationLink(destination: LearningCardView()) {
+                    Text("Learning Cards")
+                        .font(.title)
+                        .padding()
+                        .background(Color.brown)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
