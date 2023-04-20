a09f3d7cdb3ce9c2fdd911e4007b2ee3012be47c
winxene <winxeneric@gmail.com>
2023-04-20 13:42:10 +0700

    feat: added method to query the array based on the islandCategory

--- Flashcarthon.swiftpm/Models/ProvinceCityManager.swift
+++ Flashcarthon.swiftpm/Models/ProvinceCityManager.swift
@@ -1,138 +1,74 @@
 import SwiftUI
-//The map asset is taken from this repository
-//Link TBA
+//all of the picture below are taken from Oleh Oleh Uwe Dedering - Karya sendiri, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=9506522 with modifications added. This photo is intended to be used for displaying Indonesia's Provinces and its Capital Cities.
 
-/**
-class ProvinceCityManager{
- let provinces: [Province] = [
+class ProvinceCityManager: ObservableObject{
+    
+ let provinces = [
  //Sumatra (10)
-    ProvinceCity(provinceName: "Nanggroe Aceh Darussalam", capitalCityName: "Banda Aceh", islandCategory: "Sumatra", image: Image("aceh")),
-    ProvinceCity(provinceName: "Sumatra Utara (North Sumatra)", capitalCityName: "Medan", islandCategory: "Sumatra", image: Image("sumatra_utara")),
-    ProvinceCity(provinceName: "Sumatra Barat (West Sumatra)", capitalCityName: "Padang", islandCategory: "Sumatra", image: Image("sumatra_barat")),
-    ProvinceCity(provinceName: "Sumatra Selatan (South Sumatra)", capitalCityName: "Palembang", islandCategory: "Sumatra", image: Image("sumatra_selatan")),
-    ProvinceCity(provinceName: "Bangka Belitung", capitalCityName: "Pangkal Pinang", islandCategory: "Sumatra", image: Image("bangka_belitung")),
-    ProvinceCity(provinceName: "Bengkulu", capitalCityName: "Bengkulu", islandCategory: "Sumatra", image: Image("bengkulu")),
-    ProvinceCity(provinceName: "Riau", capitalCityName: "Pekanbaru", islandCategory: "Sumatra", image: Image("riau")),
-    ProvinceCity(provinceName: "Kepulauan Riau (Riau Islands)", capitalCityName: "Tanjung Pinang", islandCategory: "Sumatra", image: Image("kepulauan_riau")),
-    ProvinceCity(provinceName: "Jambi", capitalCityName: "Jambi", islandCategory: "Sumatra", image: Image("jambi")),
-    ProvinceCity(provinceName: "Lampung", capitalCityName: "Bandar Lampung", islandCategory: "Sumatra", image: Image("lampung")),
+    ProvinceCity(provinceName: "Nanggroe Aceh Darussalam", capitalCityName: "Banda Aceh", islandCategory: "Sumatra", image: "aceh"),
+    ProvinceCity(provinceName: "Sumatra Utara (North Sumatra)", capitalCityName: "Medan", islandCategory: "Sumatra", image: "sumatra_utara"),
+    ProvinceCity(provinceName: "Sumatra Barat (West Sumatra)", capitalCityName: "Padang", islandCategory: "Sumatra", image: "sumatra_barat"),
+    ProvinceCity(provinceName: "Sumatra Selatan (South Sumatra)", capitalCityName: "Palembang", islandCategory: "Sumatra", image: "sumatra_selatan"),
+    ProvinceCity(provinceName: "Bangka Belitung", capitalCityName: "Pangkal Pinang", islandCategory: "Sumatra", image: "bangka_belitung"),
+    ProvinceCity(provinceName: "Bengkulu", capitalCityName: "Bengkulu", islandCategory: "Sumatra", image: "bengkulu"),
+    ProvinceCity(provinceName: "Riau", capitalCityName: "Pekanbaru", islandCategory: "Sumatra", image: "riau"),
+    ProvinceCity(provinceName: "Kepulauan Riau (Riau Islands)", capitalCityName: "Tanjung Pinang", islandCategory: "Sumatra", image: "kepulauan_riau"),
+    ProvinceCity(provinceName: "Jambi", capitalCityName: "Jambi", islandCategory: "Sumatra", image: "jambi"),
+    ProvinceCity(provinceName: "Lampung", capitalCityName: "Bandar Lampung", islandCategory: "Sumatra", image: "lampung"),
 
-// Java (6)
-    ProvinceCity(provinceName: "Banten", capitalCityName: "Serang", islandCategory: "Java", image: Image("banten")),
-    ProvinceCity(provinceName: "Jakarta", capitalCityName: "Jakarta", islandCategory: "Java", image: Image("jakarta")),
-    ProvinceCity(provinceName: "Jawa Barat (West Java)", capitalCityName: "Bandung", islandCategory: "Java", image: Image("jawa_barat")),
-    ProvinceCity(provinceName: "Jawa Tengah (Central Java)", capitalCityName: "Semarang", islandCategory: "Java", image: Image("jawa_tengah")),
-    ProvinceCity(provinceName: "DI Yogyakarta (Yogyakarta Special Region)", capitalCityName: "Yogyakarta", islandCategory: "Java", image: Image("di_yogyakarta")),
-    ProvinceCity(provinceName: "Jawa Timur (East Java)", capitalCityName: "Surabaya", islandCategory: "Java", image: Image("jawa_timur")),
+ //Java (6)
+    ProvinceCity(provinceName: "Banten", capitalCityName: "Serang", islandCategory: "Java", image: "banten"),
+    ProvinceCity(provinceName: "Jakarta", capitalCityName: "Jakarta", islandCategory: "Java", image: "jakarta"),
+    ProvinceCity(provinceName: "Jawa Barat (West Java)", capitalCityName: "Bandung", islandCategory: "Java", image: "jawa_barat"),
+    ProvinceCity(provinceName: "Jawa Tengah (Central Java)", capitalCityName: "Semarang", islandCategory: "Java", image: "jawa_tengah"),
+    ProvinceCity(provinceName: "DI Yogyakarta (Yogyakarta Special Region)", capitalCityName: "Yogyakarta", islandCategory: "Java", image: "di_yogyakarta"),
+    ProvinceCity(provinceName: "Jawa Timur (East Java)", capitalCityName: "Surabaya", islandCategory: "Java", image: "jawa_timur"),
  
- // Lesser Sunda Islands (3)
-    ProvinceCity(provinceName: "Bali", capitalCityName: "Denpasar", islandCategory: "Lesser Sunda Islands", image: Image("bali")),
-    ProvinceCity(provinceName: "Nusa Tenggara Barat (West Nusa Tenggara)", capitalCityName: "Mataram", islandCategory: "Lesser Sunda Islands", image: Image("nusa_tenggara_barat")),
-    ProvinceCity(provinceName: "Nusa Tenggara Timur (East Nusa Tenggara)", capitalCityName: "Kupang", islandCategory: "Lesser Sunda Islands", image: Image("nusa_tenggara_timur")),
+//Lesser Sunda Islands (3)
+    ProvinceCity(provinceName: "Bali", capitalCityName: "Denpasar", islandCategory: "Lesser Sunda Islands", image: "bali"),
+    ProvinceCity(provinceName: "Nusa Tenggara Barat (West Nusa Tenggara)", capitalCityName: "Mataram", islandCategory: "Lesser Sunda Islands", image: "nusa_tenggara_barat"),
+    ProvinceCity(provinceName: "Nusa Tenggara Timur (East Nusa Tenggara)", capitalCityName: "Kupang", islandCategory: "Lesser Sunda Islands", image: "nusa_tenggara_timur"),
  
- //Borneo (5)
-    ProvinceCity(provinceName: "Kalimantan Barat (West Kalimantan)", capitalCityName: "Pontianak", islandCategory: "Borneo", image: Image("kalimantan_barat")),
-    ProvinceCity(provinceName: "Kalimantan Tengah (Central Kalimantan)", capitalCityName: "Palangka Raya", islandCategory: "Borneo", image: Image("kalimantan_tengah")),
-    ProvinceCity(provinceName: "Kalimantan Selatan (South Kalimantan)", capitalCityName: "Banjarmasin", islandCategory: "Borneo", image: Image("kalimantan_selatan")),
-    ProvinceCity(provinceName: "Kalimantan Timur (East Kalimantan)", capitalCityName: "Samarinda", islandCategory: "Borneo", image: Image("kalimantan_timur")),
-    ProvinceCity(provinceName: "Kalimantan Utara (North Kalimantan)", capitalCityName: "Tanjung Selor", islandCategory: "Borneo", image: Image("kalimantan_utara")),
+//Borneo (5)
+    ProvinceCity(provinceName: "Kalimantan Barat (West Kalimantan)", capitalCityName: "Pontianak", islandCategory: "Borneo", image: "kalimantan_barat"),
+    ProvinceCity(provinceName: "Kalimantan Tengah (Central Kalimantan)", capitalCityName: "Palangka Raya", islandCategory: "Borneo", image: "kalimantan_tengah"),
+    ProvinceCity(provinceName: "Kalimantan Selatan (South Kalimantan)", capitalCityName: "Banjarmasin", islandCategory: "Borneo", image: "kalimantan_selatan"),
+    ProvinceCity(provinceName: "Kalimantan Timur (East Kalimantan)", capitalCityName: "Samarinda", islandCategory: "Borneo", image: "kalimantan_timur"),
+    ProvinceCity(provinceName: "Kalimantan Utara (North Kalimantan)", capitalCityName: "Tanjung Selor", islandCategory: "Borneo", image: "kalimantan_utara"),
  
- //Celebes (6)
-    ProvinceCity(provinceName: "Sulawesi Utara (North Sulawesi)", capitalCityName: "Manado", islandCategory: "Celebes", image: Image("sulawesi_utara")),
-    ProvinceCity(provinceName: "Gorontalo", capitalCityName: "Gorontalo", islandCategory: "Celebes", image: Image("gorontalo")),
-    ProvinceCity(provinceName: "Sulawesi Tengah (Central Sulawesi)", capitalCityName: "Palu", islandCategory: "Celebes", image: Image("sulawesi_tengah")),
-    ProvinceCity(provinceName: "Sulawesi Selatan (South Sulawesi)", capitalCityName: "Makassar", islandCategory: "Celebes", image: Image("sulawesi_selatan")),
-    ProvinceCity(provinceName: "Sulawesi Barat (West Sulawesi)", capitalCityName: "Mamuju", islandCategory: "Celebes", image: Image("sulawesi_barat")),
-    ProvinceCity(provinceName: "Sulawesi Tenggara (South East Sulawesi)", capitalCityName: "Kendari", islandCategory: "Celebes", image: Image("sulawesi_tenggara")),
+//Celebes (6)
+    ProvinceCity(provinceName: "Sulawesi Utara (North Sulawesi)", capitalCityName: "Manado", islandCategory: "Celebes", image: "sulawesi_utara"),
+    ProvinceCity(provinceName: "Gorontalo", capitalCityName: "Gorontalo", islandCategory: "Celebes", image: "gorontalo"),
+    ProvinceCity(provinceName: "Sulawesi Tengah (Central Sulawesi)", capitalCityName: "Palu", islandCategory: "Celebes", image: "sulawesi_tengah"),
+    ProvinceCity(provinceName: "Sulawesi Selatan (South Sulawesi)", capitalCityName: "Makassar", islandCategory: "Celebes", image: "sulawesi_selatan"),
+    ProvinceCity(provinceName: "Sulawesi Barat (West Sulawesi)", capitalCityName: "Mamuju", islandCategory: "Celebes", image: "sulawesi_barat"),
+    ProvinceCity(provinceName: "Sulawesi Tenggara (South East Sulawesi)", capitalCityName: "Kendari", islandCategory: "Celebes", image: "sulawesi_tenggara"),
  
- //Maluku (2)
-    ProvinceCity(provinceName: "Maluku", capitalCityName: "Ambon", islandCategory: "Maluku", image: Image("maluku")),
-    ProvinceCity(provinceName: "Maluku Utara (North Maluku)", capitalCityName: "Sofifi", islandCategory: "Maluku", image: Image("maluku_utara")),
+//Maluku (2)
+    ProvinceCity(provinceName: "Maluku", capitalCityName: "Ambon", islandCategory: "Maluku", image: "maluku"),
+    ProvinceCity(provinceName: "Maluku Utara (North Maluku)", capitalCityName: "Sofifi", islandCategory: "Maluku", image: "maluku_utara"),
  
- //Papua (6)
-    ProvinceCity(provinceName: "Papua", capitalCityName: "Jayapura", islandCategory: "Papua", image: Image("papua"))
-    ProvinceCity(provinceName: "Papua Barat (West Papua)", capitalCityName: "Manokwari", islandCategory: "Papua", image: Image("papua_barat")),
-    ProvinceCity(provinceName: "Papua Barat Daya (North West Papua)", capitalCityName: "Sorong", islandCategory: "Papua", image: Image("papua_barat_daya"))
-    ProvinceCity(provinceName: "Papua Pegunungan", capitalCityName: "Jayawijaya", islandCategory: "Papua", image: Image("papua_pegunungan"))
-    ProvinceCity(provinceName: "Papua Selatan", capitalCityName: "Merauke", islandCategory: "Papua", image: Image("papua_selatan"))
-    ProvinceCity(provinceName: "Papua Tengah", capitalCityName: "Nabire", islandCategory: "Papua", image: Image("papua_tengah"))
+//Papua (6)
+    ProvinceCity(provinceName: "Papua", capitalCityName: "Jayapura", islandCategory: "Papua", image: "papua"),
+    ProvinceCity(provinceName: "Papua Barat (West Papua)", capitalCityName: "Manokwari", islandCategory: "Papua", image: "papua_barat"),
+    ProvinceCity(provinceName: "Papua Barat Daya (North West Papua)", capitalCityName: "Sorong", islandCategory: "Papua", image: "papua_barat_daya"),
+    ProvinceCity(provinceName: "Papua Pegunungan", capitalCityName: "Jayawijaya", islandCategory: "Papua", image: "papua_pegunungan"),
+    ProvinceCity(provinceName: "Papua Selatan", capitalCityName: "Merauke", islandCategory: "Papua", image: "papua_selatan"),
+    ProvinceCity(provinceName: "Papua Tengah", capitalCityName: "Nabire", islandCategory: "Papua", image: "papua_tengah")
  ]
  
- func getRandomProvince() -> (provinceName: String, capitalCity: String, image: Image) {
+ func getRandomProvince() -> (provinceName: String, capitalCityName: String, image: String) {
      let randomIndex = Int.random(in: 0..<provinces.count)
      let province = provinces[randomIndex]
      return (province.provinceName, province.capitalCityName, province.image)
  }
-}
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class ProvinceCityManager {
-    let provincesAndCapitals = [
-//Sumatra
-        "Aceh": "Banda Aceh",        
-        "Sumatera Utara": "Medan",
-        "Sumatera Barat": "Padang",
-        "Kepulauan Riau": "Tanjung Pinang",
-        "Jambi": "Jambi",
-        "Bangka Belitung": "Pangkal Pinang",
-        "Bengkulu": "Bengkulu",
-        "Riau": "Pekanbaru",
-        "Sumatera Selatan": "Palembang",
-        "Lampung": "Bandar Lampung",
-//Java    
-        "DKI Jakarta": "Jakarta",
-        "DI Yogyakarta": "Yogyakarta",
-        "Banten": "Serang",
-        "Jawa Barat": "Bandung",
-        "Jawa Tengah": "Semarang",
-        "Jawa Timur": "Surabaya",
-        "Bali": "Denpasar",
-        "Nusa Tenggara Barat": "Mataram",
-        "Nusa Tenggara Timur": "Kupang",
-//Kalimantan (borneo)        
-        "Kalimantan Barat": "Pontianak",
-        "Kalimantan Selatan": "Banjarmasin",
-        "Kalimantan Tengah": "Palangkaraya",
-        "Kalimantan Timur": "Samarinda",
-        "Kalimantan Utara": "Tanjung Selor",
-//Sulawesi
-        "Gorontalo": "Gorontalo",
-        "Sulawesi Barat": "Mamuju",
-        "Sulawesi Selatan": "Makassar",
-        "Sulawesi Tengah": "Palu",
-        "Sulawesi Tenggara": "Kendari",
-        "Sulawesi Utara": "Manado",
-//Maluku 
-        "Maluku": "Ambon",
-        "Maluku Utara": "Sofifi",
-//Papua
-        "Papua": "Jayapura",
-        "Papua Barat": "Manokwari",
-        "Papua Barat Daya": "Sorong",
-        "Papua Pegunungan": "Jayawijaya",
-        "Papua Selatan": "Merauke",
-        "Papua Tengah": "Nabire",
-    ]
-    
-    func getRandomProvince() -> (province: String, capital: String) {
-        let index = Int.random(in: 0..<provincesAndCapitals.count)
-        let province = Array(provincesAndCapitals.keys)[index]
-        let capital = Array(provincesAndCapitals.values)[index]
-        return (province, capital)
+ func queryByIslandCategory(_ category: String) -> [(provinceName: String, capitalCityName: String, image: String)] {
+        var results: [(provinceName: String, capitalCityName: String, image: String)] = []
+        for province in provinces {
+            if province.islandCategory == category || category == "All" {
+                results.append((province.provinceName, province.capitalCityName, province.image))
+            }
+        }
+        return results
     }
 }
