4b031ccf9ba05d1b70be713a6d73ccdb02e1c153
winxene <winxeneric@gmail.com>
2023-04-20 13:39:58 +0700

    fix: fix button won't sort the learning cards

--- Flashcarthon.swiftpm/LearningCard/LearningCard.swift
+++ Flashcarthon.swiftpm/LearningCard/LearningCard.swift
@@ -3,30 +3,33 @@ import SwiftUI
 struct LearningCard: View {
     var province: String
     var capitalCity: String
-//    var image: Image
+    var image: String    
     
     var body: some View {
-        HStack(spacing: 10) {
-//            Image(UIImage: image)
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 80, height: 80)
+        VStack(spacing: 10) {
+            Image("\(image)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
 //                .clipShape(Circle())
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(province)
                     .font(.headline)
-                
+                    .multilineTextAlignment(.center)
+                    .frame(width: 100)
                 Text(capitalCity)
                     .font(.subheadline)
                     .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 100)
             }
-            
             Spacer()
         }
         .padding()
         .background(Color.white)
         .cornerRadius(10)
         .shadow(radius: 5)
+        .frame(width: 200)
     }
 }
--- Flashcarthon.swiftpm/LearningCard/View/LearningCardView.swift
+++ Flashcarthon.swiftpm/LearningCard/View/LearningCardView.swift
@@ -3,8 +3,19 @@ import SwiftUI
 struct LearningCardView: View {
     let provinceCityManager = ProvinceCityManager()
     @State private var selectedRegion: String = "Sumatra"
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
+    var columns: [GridItem] {
+        if horizontalSizeClass == .compact {
+            return [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+        } else {
+            return [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+        }
+    }
+
     var body: some View {
         VStack {
+            //all of the picture below are taken from https://www.youtube.com/watch?v=DYndZw5f_8M with modifications added.
             ButtonRow(selectedRegion: $selectedRegion, buttons: [
                 ButtonInfo(title: "All", image: "island_all"),
                 ButtonInfo(title: "Sumatra", image: "island_sumatra"),
@@ -13,16 +24,17 @@ struct LearningCardView: View {
                 ButtonInfo(title: "Borneo", image: "island_borneo"),
                 ButtonInfo(title: "Celebes", image: "island_celebes"),
                 ButtonInfo(title: "Papua", image: "island_papua")
-            ], buttonWidth: 80, buttonHeight: 50)
-            }
+            ])
+            
             ScrollView {
-                VStack(spacing: 20){
-                    ForEach(provinceCityManager.provincesAndCapitals.sorted(by: <), id: \.key) { province, capital in
-                        LearningCard(province: province, capitalCity: capital)
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(provinceCityManager.queryByIslandCategory(selectedRegion), id: \.provinceName)
+                    {province in
+                        LearningCard(province: province.provinceName, capitalCity: province.capitalCityName, image: province.image)
                     }
-                    .padding()
                 }
+                .padding()
             }
         }
     }
-
+}
