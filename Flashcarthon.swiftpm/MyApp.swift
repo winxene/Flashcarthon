dd4caf09cbefa31e03ab017292c3943e4b7cad08
winxene <winxeneric@gmail.com>
2023-04-20 13:42:43 +0700

    refactor: made the default app in landscape

--- Flashcarthon.swiftpm/MyApp.swift
+++ Flashcarthon.swiftpm/MyApp.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import PlaygroundSupport
 
 @main
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
               HomeView()
+                .onAppear {
+                    UIDevice.current.setValue(UIInterfaceOrientation.landscapeRight.rawValue, forKey: "orientation")
+                }
         }
     }
 }
